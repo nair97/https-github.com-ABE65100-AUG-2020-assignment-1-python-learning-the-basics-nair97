042918b970b1ced6e16aab87ce8f1b99f7e6edd9
nair97 <70231014+nair97@users.noreply.github.com>
2020-08-27 07:34:49 -0400

    Q3.2 done
    Exercise 3.2 done

---
+++ template_Exercise_3.2_do_four.py
@@ -0,0 +1,11 @@
+"""
+Delete this message, and add your personal header comment here... 
+Don't forget to add in-line comments...
+"""
+
+def do_twice(func,arg):
+    def print_twice(arg):
+        def do_four(func,arg):
+            do_twice(print, ' spam')
+            do_four(print, 'spam')
+            3 done
