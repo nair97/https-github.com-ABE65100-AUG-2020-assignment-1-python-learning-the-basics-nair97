df9ed463f65c2c82d5489a8077f46a9bdb3ff37d
nair97 <70231014+nair97@users.noreply.github.com>
2020-09-02 14:32:39 -0400

    new done
    new changes done

---
+++ Exercise_6_5_gcd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 16:05:10 2020
+
+@author: meerarakesh09
+"""
+#The greatest common divisor (GCD) of a and b is the largest number that 
+#divides both of them with no remainder.
+
+def gcd(a, b):
+   # a, b: positive integers
+    #returns: a positive integer, the greatest common divisor of a & b.
+    # Base case is when b = 0
+    r = a % b
+    if b == 0:
+        return a
+    else:
+        return gcd(b, r) #calculate gcd value
+    
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(gcd(num1,  num2))
+    
---
+++ Exercise_7_1_sqrt.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 16:05:10 2020
+
+@author: meerarakesh09
+"""
+
+# import math module
+import math
+
+#functions and conditional loops to calculate newton's square root method
+def mysqrt(a):
+    x = a/2
+    while True:
+        y = (x + a/x)/2
+        if y == x:
+           return y
+           break
+        x = y
+
+#to test and print table using loop format
+def test_square_root(num_a):
+  #construct spacing between columns
+    sp1 = "  "
+    sp2 = "  " * 3
+    sp3 = " "
+#to list table headers and columns
+    print("a", sp1, "mysqrt(a)", sp2, "math.sqrt(a)", sp3, "diff")
+    print("---", sp1, "----------", sp2, "-------------", sp3, "------") # to fill space and underline the headers
+#loop to calculate the square root method and print according to the table format in columns
+    for a in range(num_a):
+        c1 = float(a)
+        c2 = mysqrt(a)
+        c3 = math.sqrt(a)
+        c4 = abs(mysqrt(a) - math.sqrt(a))
+        print(c1, "{:<10f}".format(c2), "{:<12f}".format(c3), c4)
+    test_square_root(range(1,10))
+    
