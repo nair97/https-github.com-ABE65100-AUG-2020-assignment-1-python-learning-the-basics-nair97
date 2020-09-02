3fde6eb9c51abf62aba27c989de309315cf7eed4
nair97 <70231014+nair97@users.noreply.github.com>
2020-09-02 06:24:19 -0400

    Update Exercise_5.2_check_fermat.py
    fermat change new

--- Exercise_5.2_check_fermat.py
+++ Exercise_5.2_check_fermat.py
@@ -8,17 +8,19 @@ Write function to check fermat theorem that:
 a**n + b**n = c**n works or not
 """
   # to check if fermat theorem formula works or not
-def check_fermat(a,b,c,n):  
-    if (n>2) and (a**n + b**n == c**n):
-        print("Holy smoke!Fermat was wrong!")
+def check_fermat(a, b, c, n):   #Fermat's Last Theorem Function takes 4 inputs and check whether Fermat's Last Theorem is valid
+#a,b,c : Positive integers n: Any value greater than n
+    if n > 2 and (a**n + b**n == c**n):
+        print("Holy smokes, Fermat was wrong!")
     else:
-        print("No! that doesnt work")
+        print("No, that doesn’t work.")
+       
  # to input values for the parameters and return check_fermat 
-def check_number():
+def check_numbers():
     a = int(input("Choose a number for a: "))
     b = int(input("Choose a number for b: "))
     c = int(input("Choose a number for c: "))
     n = int(input("Choose a number for n: "))
-    return check_fermat(a,b,c,n)
+    return check_fermat(a, b, c, n)
 
-check_number()
+check_numbers()
