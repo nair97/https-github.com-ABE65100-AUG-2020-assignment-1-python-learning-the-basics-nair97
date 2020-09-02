22028560595c908c2b8b6c916df0a1236e660762
nair97 <70231014+nair97@users.noreply.github.com>
2020-09-02 05:27:15 -0400

    new flower
    flower change new

--- Exercise_4.2_flower.py
+++ Exercise_4.2_flower.py
@@ -10,6 +10,9 @@ import math
 import turtle
 #math function provides all mathematical functions
 #turtle module creates images
+# import the tkinter graphics library tools.  Note that is was called Tkinter for
+# Python 2
+from tkinter import *
 
 def polyline(obj, length, sides, angle):
     for i in range(sides):
@@ -52,23 +55,26 @@ def move_turtle(t, length):
     #moving pen up and down using turtle to draw the flower
 
 bob = turtle.Turtle()
+# the following condition checks whether we are
+# running as a script, in which case run the test code,
+# or being imported, in which case don't.
 
-move_turtle(bob, -200)
-flower(bob, 7, 60.0, 60.0)
+if __name__ == '__main__':
 
-move_turtle(bob, 200)
-flower(bob, 10, 50.0, 70.0)
-
-move_turtle(bob, 200)
-flower(bob, 20, 120.0, 20.0)
+    # Indent your control code here so that it does not
+    # run if functions are imported by another program.
+# draw the first flower
+  move_turtle(bob, -200)
+  flower(bob, 7, 60.0, 60.0)
+ # move to next location, draw second flower
+  move_turtle(bob, 200)
+  flower(bob, 10, 50.0, 70.0)
+# move to next location, draw third flower
+  move_turtle(bob, 200)
+  flower(bob, 20, 120.0, 20.0)
 
 bob.hideturtle()
-turtle.mainloop()
 
-
-# After drawing the three flowers: DO NOT include the command turtle.done()
-# or turtle.mainloop() - these commands just freeze the open window.  Instead
-# add the following sequence of commands.
 ts = turtle.getscreen() # grab the drawing screen for later use
 ts.getcanvas().postscript(file="flower.eps") # extract the image and save as a postscript file
 turtle.bye() # close the drawing screen and end the turtle session
